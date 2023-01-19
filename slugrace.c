0db9fd513cffb575937aa54d600edd77d52263d4
JakeLinux <jake.leavitt74@gmail.com>
2023-01-19 14:23:57 -0500

    completed slugrace

--- slugrace.c
+++ slugrace.c
@@ -14,6 +14,9 @@ double getRaceTime(struct timespec, struct timespec);
 
 int main(){
 
+    struct timespec start, stop;
+    clock_gettime(CLOCK_REALTIME, &start);
+    printf("START: %lf\n", start.tv_nsec);
     int parent = getpid();
     int children[4];
     int numChild;
@@ -24,48 +27,49 @@ int main(){
         }
     }
     if(getpid() != parent){
-        char program[80], *args[3];
-        strcpy(program, "slug");
-        args[0] = "slug";
-        args[1] = numChild;
+        char program[80], *args[3], num[10];
+        strcpy(program, "./slug");
+        sprintf(num, "%d", numChild);
+        args[0] = "./slug";
+        args[1] = num;
         args[2] = NULL;
-        printf("[Child, PID: %d] Executing './slug %d' command...\n", getpid(), args[1]);
+        printf("[Child, PID: %d] Executing './slug %d' command...\n", getpid(), numChild);
         execvp(program, args);
     }
 
     bool race = true;
-    struct timespec start, stop;
-    clock_gettime(CLOCK_REALTIME, &start);
 
     if(getpid() == parent){
         while(race){
-            usleep(3300000);
+            usleep(330000);
             race = false;
             char result[120];
             strcpy(result, "The race is still ongoing. Children still racing: ");
             for(int i = 0; i < 4; i++){
                 int status;
                 int cpid = waitpid(children[i], &status, WNOHANG);
-                printf("CHILD: %d, STATUS: %d, CPID: %d\n",children[i], status, cpid);
-                if(status >= 0){
+                //printf("CHILD: %d, STATUS: %d, CPID: %d\n",children[i], status, cpid);
+                if(cpid == 0){
                     char buffer[10];
                     race = true; //One or more still alive
                     sprintf(buffer, "%d", children[i]);
 				    strcat(result, buffer);
 				    strcat(result, " ");
                 }
-                else if(cpid == -100){
+                else if(cpid != -1 && cpid != 0){
                     struct timespec childStop;
                     clock_gettime(CLOCK_REALTIME, &childStop);
                     double finishTime;
                     finishTime = getRaceTime(start, childStop);
-                    printf("Child %d has crossed the finish line! It took %lf seconds.\n", cpid, finishTime);
+                    printf("Child %d has crossed the finish line! It took %lf seconds.\n", children[i], finishTime);
 
                 }
 
             }
-            strcat(result, "\n");
-            printf("%s", result);
+            if(race){
+                strcat(result, "\n");
+                printf("%s", result); 
+            }
         }
 
 
@@ -73,12 +77,13 @@ int main(){
         clock_gettime(CLOCK_REALTIME, &stop);
         double raceTime;
         raceTime = getRaceTime(start, stop);
+        printf("The race is over! It took %lf seconds!\n", raceTime);
     }
 
 }
 
 double getRaceTime(struct timespec start, struct timespec stop){
-    double finish = ((stop.tv_sec - start.tv_sec) + (stop.tv_nsec - start.tv_nsec)) / 1000000000;
+    double finish = (stop.tv_sec - start.tv_sec) + ((stop.tv_nsec - start.tv_nsec)/1000000000);
     return finish;
 }
 
